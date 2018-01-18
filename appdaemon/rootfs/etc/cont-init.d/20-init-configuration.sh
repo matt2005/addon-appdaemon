61e5d9c76926f2cb97419255186fa3dc621a9772
matt <matthilton2005@gmail.com>
2018-01-18 23:00:41 +0000

    test1

--- appdaemon/rootfs/etc/cont-init.d/20-init-configuration.sh
+++ appdaemon/rootfs/etc/cont-init.d/20-init-configuration.sh
@@ -10,3 +10,9 @@ if ! hass.directory_exists '/config/appdaemon'; then
     cp -R /root/appdaemon /config/appdaemon \
         || hass.die 'Failed to create initial AppDaemon configuration'
 fi
+
+case "$(hass.config.get 'appdaemon_version')" in
+    3.*
+        mv /config/appdaemon/apps.yaml /config/appdaemon/apps/ \
+        || hass.die 'Failed to Move apps.yaml to apps folder'
+esac
